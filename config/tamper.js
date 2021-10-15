6c46303a9ff6f901e263c13d6977b953c2ab3270
Yuhao Yan <yuhaoyan@microsoft.com>
2021-10-15 15:23:28 +0800

    vue

--- config/tamper.js
+++ config/tamper.js
@@ -4,7 +4,8 @@ const config = {
   description: 'hello world!',
   author: 'flasco',
   grant: ['GM_xmlhttpRequest', 'GM_setValue', 'GM_getValue', 'unsafeWindow'],
-  match: ['www.baidu.com'],
+  match: ['*'],
+  include: ['*'],
   require: [
     'https://cdn.bootcdn.net/ajax/libs/react/17.0.2/umd/react.production.min.js',
     'https://cdn.bootcdn.net/ajax/libs/react-dom/17.0.2/umd/react-dom.production.min.js',
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import ReactDOM from 'react-dom';
 
 import App from './index';
 import { inject } from './utils/dom-inject';
-
+console.log('-====111122222222')
 inject(
   () => {
+    console.log('-====1111')
     const group = document.querySelector('body');
     if (group == null) return null;
 
     const ct = document.createElement('div');
+    ct.id = 'react'
     group.append(ct);
     return ct;
   },
   el => {
+    console.log('-====11133331')
     ReactDOM.render(React.createElement(App), el);
   }
 );
